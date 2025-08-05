a9bde031b29dffe012e73048d3dceb7928fbaf4e
Tridibesh  Sastri <tridibeshs30@gmail.com>
2025-08-05 16:08:32 +0530

    first test with 01vitereact

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
-
+import Chai from "./chai"
 
 function App() {
 
 
   return (
     <>
-      <h1>hi I'm Trixion here writing react with vite </h1>
+      {/* <h1>hi I'm Trixion here writing react with vite </h1> */}
+      <Chai/>
     </>
   )
 }
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+    <h3>Hi this is chai in h3 test for random function return by render by main.js</h3>)
+}
+
+export default Chai
