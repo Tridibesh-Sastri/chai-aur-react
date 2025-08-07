1e6073b319cf0a1c88e96a6a98fb4799df3aa098
Tridibesh  Sastri <tridibeshs30@gmail.com>
2025-08-08 05:19:50 +0530

    building a chep version of react

--- 01react/src/App.js
+++ 01react/src/App.js
@@ -1,7 +1,11 @@
+import Chai from './chai'
 
 function App() {
   return (
+    <>
     <h1>Hello! dosto it's tennessine here</h1>
+    <Chai/>
+    </>
   );
 }
 
---
+++ 01react/src/chai.js
@@ -0,0 +1,4 @@
+function Chai(){
+    return <h1> Hi now I'm inside basic react without vite </h1>
+}
+export default Chai
